809dfe9f6f57430290e4021564bc0ccab3a0ed9d
Zoe Ben Bogner <153617265+zoebbogner@users.noreply.github.com>
2025-05-15 23:16:06 +0300

    Removed URL related staff

--- main_config.py
+++ main_config.py
@@ -2,20 +2,14 @@
 from pathlib import Path
 
 # === File and Directory Paths ===
-MODEL_PATH = "models/url_model/results/model.pkl"
-EXPORT_JSON_PATH = "models/url_model/results/logreg_weights.json"
-FEATURE_IMPORTANCE_PATH = "models/url_model/results/feature_importance.png"
-CLASSIFICATION_REPORT_PATH = "models/url_model/results/classification_report.txt"
-PROCESSED_PATH = "data/processed/full_feature_set.csv"
 RAW_DIR = "data/raw"
-PRODUCTION_PATH = "models/url_model/production/url_model.pkl"
-
 BENIGN_CSV: str = "benign_urls.csv"
 OPENPHISH_CSV = "openphish.csv"
 PHISHTANK_CSV = "phishtank.csv"
 ALL_URLS_CSV = "all_urls.csv"
 SYNTHETIC_ALL_URLS_CSV = "synthetic_all_urls.csv"
 RAW_FILES = [BENIGN_CSV, OPENPHISH_CSV, PHISHTANK_CSV]
+
 def get_benign_csv_dir(synthetic_urls: bool = False) -> str:
     """Get the path to the benign CSV file based on synthetic URLs flag."""
     if synthetic_urls:
